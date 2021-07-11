f94d7ecb6742971d50c2f00727d320b298822ff7
nagasai6 <nagasai317@gmail.com>
2021-07-11 11:39:32 +0530

    ui-updates

--- public/js/home.js
+++ public/js/home.js
@@ -4,12 +4,40 @@ var __webpack_exports__ = {};
   !*** ./src/home.js ***!
   \*********************/
 $(function () {
+  var starttimeOptions = {
+    vibrate: true,
+    donetext: 'Done',
+    placement: 'bottom',
+    "default": 'now',
+    align: 'right',
+    autoclose: true
+  };
+  var endtimeOptions = {
+    vibrate: true,
+    donetext: 'Done',
+    placement: 'bottom',
+    align: 'left',
+    autoclose: true
+  };
   var datepickerOptions = {
     dateFormat: "dd/mm/yy",
     minDate: new Date(),
     maxDate: "+1m"
   };
   $("#datepicker").datepicker(datepickerOptions);
+  $("#endtime").clockpicker(endtimeOptions);
+  $('#starttime').clockpicker(starttimeOptions);
 });
+$("#availabilityCheckForm").on("submit", function (e) {
+  e.preventDefault();
+  collectDataFromForm();
+});
+
+function collectDataFromForm() {
+  var queryCategory = $("#searchInput").val();
+  var queryDate = $("#datepicker").val();
+  var queryStart = $("#starttime").val();
+  var queryEnd = $("#endtime").val();
+}
 /******/ })()
 ;
--- src/home.js
+++ src/home.js
@@ -1,13 +1,46 @@
-
 $( function() {
-  let datepickerOptions ={
-    dateFormat:"dd/mm/yy",
-    minDate : new Date(),
-    maxDate : "+1m"
-  }
-    $( "#datepicker" ).datepicker(datepickerOptions);
+let starttimeOptions ={
+  vibrate:true,
+  donetext:'Done',
+  placement:'bottom',
+  default:'now',
+  align:'right',
+  autoclose:true
+};
+let endtimeOptions = { 
+  vibrate:true,
+  donetext:'Done',
+  placement:'bottom',
+  align:'left',
+  autoclose:true
+};
+let datepickerOptions ={
+  dateFormat:"dd/mm/yy",
+  minDate : new Date(),
+  maxDate : "+1m"
+};
+
+  $( "#datepicker" ).datepicker(datepickerOptions);
+    $("#endtime").clockpicker(endtimeOptions);
+    $('#starttime').clockpicker(starttimeOptions);
+
   } );
 
+  $("#availabilityCheckForm").on("submit",(e)=>{
+      e.preventDefault();
+      collectDataFromForm();
+  })
+
+  function collectDataFromForm(){
+   let queryCategory = $("#searchInput").val();
+   let queryDate = $("#datepicker").val();
+   let queryStart = $("#starttime").val();
+   let queryEnd = $("#endtime").val();
+ 
+  }
+
+ 
+  
 
 
 
