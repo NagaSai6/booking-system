de999e6796a9b0b0758b308dfe0552b6f4442391
nagasai6 <nagasai317@gmail.com>
2021-07-17 11:09:06 +0530

    compress-js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require('laravel-mix');
 mix.disableSuccessNotifications();
 mix.js('src/admin.js', 'public/js/admin.js');
 mix.js('src/home.js','public/js/home.js');
-mix.js('src/admin-1.js','public/js/admin-1.js');
 
 
 
